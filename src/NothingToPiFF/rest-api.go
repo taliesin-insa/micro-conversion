a061ffb3dd50dc52dfe6909e3cd72a3c1c6eaf49
Zoé Levrel <zoe.levrel@insa-rennes.fr>
2020-01-19 11:53:13 +0100

    fix(API): change server port, update request body format (string to json)

--- src/NothingToPiFF/rest-api.go
+++ src/NothingToPiFF/rest-api.go
@@ -1,34 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type RequestData struct {
+	FilePath string
+}
+
 func generatePiFF(w http.ResponseWriter, r *http.Request) {
+	// get request body
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	result := convertListToPiFF(string(reqBody))
+	// converts body to json
+	var reqData RequestData
+	err = json.Unmarshal(reqBody, &reqData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, err = w.Write([]byte("Wrong request body format: this microservice needs a JSON which attribute 'FilePath' is a string"))
+		return
+	}
 
-	// get struct version
-	//var piFFResult PiFFList
-	//err = json.Unmarshal(result, &piFFResult)
-	//checkError(err)
+	// get PiFF list
+	result := convertListToPiFF(reqData.FilePath)
 
+	// send answer
 	w.WriteHeader(http.StatusOK)
 	_, err = w.Write(result)
 	checkError(err)
 }
 
+// function to test whether docker file is correctly built
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
 func main() {
 	// Define the routing
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/convert", generatePiFF).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/convert/nothing", generatePiFF).Methods("POST")
+	router.HandleFunc("/", homeLink).Methods("GET")
+	log.Fatal(http.ListenAndServe(":12345", router))
 }

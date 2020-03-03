39d359b58d08065eb93a6dad13645308babb61b4
Zoé Levrel <zoe.levrel@insa-rennes.fr>
2020-03-03 11:18:03 +0100

    refactor(Conversion): put all conversion code into rest-api.go file

--- src/Conversion/rest-api.go
+++ src/Conversion/rest-api.go
@@ -4,10 +4,45 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
+
+type RequestDataNothing struct {
+	Path string
+}
+
+type Meta struct {
+	Type string
+	URL  string
+}
+
+type Location struct {
+	Type    string
+	Polygon [][2]int
+	Id      string
+}
+
+type Data struct {
+	Type       string
+	LocationId string
+	Value      string
+	Id         string
+}
+
+type PiFFStruct struct {
+	Meta     Meta
+	Location []Location
+	Data     []Data
+	Children []int
+	Parent   int
+}
 
 func generatePiFF(w http.ResponseWriter, r *http.Request) {
 	// get request body
@@ -15,7 +50,6 @@ func generatePiFF(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
-		fmt.Println(err.Error())
 		return
 	}
 
@@ -24,22 +58,72 @@ func generatePiFF(w http.ResponseWriter, r *http.Request) {
 	err = json.Unmarshal(reqBody, &reqData)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Wrong request body format: this microservice needs a JSON which attribute 'Path' is a string"))
+		w.Write([]byte(err.Error()))
 		return
 	}
 
-	// get PiFF list
-	result, err := convertFromNothingToPiFF(reqData.Path)
+	// get dimensions of image
+	file, err := os.Open(reqData.Path)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	image, _, err := image.DecodeConfig(file)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	width := image.Width
+	height := image.Height
+
+	err = file.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// fill PiFF struct
+	PiFFData := PiFFStruct{
+		Meta: Meta{
+			Type: "line",
+			URL:  "",
+		},
+		Location: []Location{
+			{Type: "line",
+				Polygon: [][2]int{
+					{0, 0},
+					{height, 0},
+					{height, width},
+					{0, width},
+				},
+				Id: "loc_0",
+			},
+		},
+		Data: []Data{
+			{
+				Type:       "line",
+				LocationId: "loc_0",
+				Value:      "",
+				Id:         "0",
+			},
+		},
+	}
+
+	result, err := json.MarshalIndent(PiFFData, "", "     ")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		fmt.Println(err.Error())
 		return
-
-	} else {
-		w.WriteHeader(http.StatusOK)
-		w.Write(result)
 	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
 }
 
 // function to test whether docker file is correctly built
